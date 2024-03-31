aaf72bea458366048b7b0e9408c6114526def0be
bhattritik21 <bhattritik21@gmail.com>
2024-03-31 12:47:42 +0530

    added responsiveness2

--- app/components/TodoList.js
+++ app/components/TodoList.js
@@ -1,7 +1,7 @@
 "use client"
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import { useSelector, useDispatch } from 'react-redux';
-import { toggleTodo, deleteTodo ,editTodo} from '../redux/todoActions';
+import { toggleTodo, deleteTodo, editTodo } from '../redux/todoActions';
 
 const TodoList = () => {
   const todos = useSelector(state => state.todos);
@@ -46,13 +46,13 @@ const TodoList = () => {
     <div className='todoList w-3/5 p-40'>
       <h1 className='text-text font-medium text-2xl'>To do tasks</h1>
       <div className='flex mt-8 mb-8'>
-        <button className='text-text py-1 w-24 mx-4 rounded-lg border-2 border-text' onClick={() => setFilter('ALL')}>All</button>
-        <button className='text-text py-1 w-24 mx-4 rounded-lg border-2 border-text ' onClick={() => setFilter('COMPLETED')}>Done</button>
-        <button className='text-text py-1 w-24 mx-4 rounded-lg border-2 border-text' onClick={() => setFilter('ACTIVE')}>Todo</button>
+        <button className='btn text-text py-1 w-24 mx-4 rounded-lg border-2 border-text' onClick={() => setFilter('ALL')}>All</button>
+        <button className='btn text-text py-1 w-24 mx-4 rounded-lg border-2 border-text ' onClick={() => setFilter('COMPLETED')}>Done</button>
+        <button className='btn text-text py-1 w-24 mx-4 rounded-lg border-2 border-text' onClick={() => setFilter('ACTIVE')}>Todo</button>
       </div>
       <ul>
         {filteredTodos.map(todo => (
-          <li key={todo.id} className='border-bc border-2 rounded p-3 mt-2'>
+          <li key={todo.id} className='list flex justify-between border-bc border-2 rounded p-3 mt-2'>
             {editableTodoId === todo.id ? (
               <>
                 <input
@@ -60,22 +60,31 @@ const TodoList = () => {
                   value={editedText}
                   onChange={event => setEditedText(event.target.value)}
                 />
-                 <button className='bg-red text-white px-2 mx-2 rounded float-right' onClick={handleCancelEdit}>Cancel</button>
-                <button className='bg-text text-white px-2 mx-2 rounded float-right' onClick={() => handleSaveEdit(todo.id)}>Save</button>          
+                <div className='buttons'>
+                  <button className='bg-red text-white px-2 mx-2 rounded' onClick={handleCancelEdit}>Cancel</button>
+                  <button className='bg-text text-white px-2 mx-2 rounded' onClick={() => handleSaveEdit(todo.id)}>Save</button>
+                </div>
+
               </>) : (
               <>
-                <input
-                  className='mx-2'
-                  type="checkbox"
-                  checked={todo.completed}
-                  onChange={() => handleToggle(todo.id)}
-                />
-                <span style={{ textDecoration: todo.completed ? 'line-through' : 'none',color: todo.completed ? 'gray' : 'black' }}>
-                  {todo.text}
-                </span>
-                <button className='bg-red text-white px-2 mx-2 rounded float-right' onClick={() => handleDelete(todo.id)}>Delete</button>
-                <button className='bg-text text-white px-2 mx-2 rounded float-right' onClick={() => handleEdit(todo.id, todo.text)}>Edit</button>
-                </>)}
+                <div>
+                  <input
+                    className='mx-2'
+                    type="checkbox"
+                    checked={todo.completed}
+                    onChange={() => handleToggle(todo.id)}
+                  />
+                  <span style={{ textDecoration: todo.completed ? 'line-through' : 'none', color: todo.completed ? 'gray' : 'black' }}>
+                    {todo.text}
+                  </span>
+                </div>
+
+                <div className='buttons'>
+                  <button className='bg-red text-white px-2 mx-2 rounded' onClick={() => handleDelete(todo.id)}>Delete</button>
+                  <button className='bg-text text-white px-2 mx-2 rounded' onClick={() => handleEdit(todo.id, todo.text)}>Edit</button>
+                </div>
+
+              </>)}
 
           </li>
         ))}
